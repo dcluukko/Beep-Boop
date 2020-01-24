854be6742f15fa465227bcba1d7983be664f6dc7
Dana Luukko <Dana Luukko>
2020-01-24 15:46:50 -0800

    It will have a continuation of figuring out logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 
-// var Userinput = function (newArray) {
-//     var blankArray = []
-
-//     for (var i = 0; i <= newArray; i++) {
-//         blankArray.push(i);
-//         console.log(blankArray);
-//     }
-// }
-
-
+var blankArray = []
+var heresOne;
+var userInput = function(forArray) {
+for (var i = 0; i <= forArray; i++) {
+    blankArray.push(i);
+    return blankArray
+    
+  }
+};
+var findOne = function(numberInput) {
+    var rangeArray = numberInput;
+    for ( var i = 0; i <= numberInput; i ++ ){
+        console.log(rangeArray);
+      if (rangeArray.includes("13") ){
+        alert("truth!");
+      }
+    }       
+  };
+  
 
 
 
@@ -16,18 +25,10 @@ $(document).ready(function () {
     $("form#range-returner").submit(function (event) {
         event.preventDefault();
         var input = parseInt($("input#number").val());
+        input = userInput(input)
+        input = findOne(input); 
+          
         
-            var blankArray = []
-
-            for (var i = 0; i <= input; i++) {
-                blankArray.push(i);
-                var arraySplit = blankArray.slice();
-                if (arraySplit.includes([1]) ) {
-                    $("#beep").show();
-                }
-                // $("#response").text(blankArray); 
-
-            }
           
         
 
